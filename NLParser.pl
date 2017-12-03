a9b3f6a8a889b6614e768116791ba49630ebf950
Jared <jared@innitel.com>
2017-12-03 13:38:13 -0500

    added in input

--- NLParser.pl
+++ NLParser.pl
@@ -72,4 +72,28 @@ isSentence(X,A,B,C):-
     isVerbPhrase(A,B,C).
 
 
-    
+ main :-
+    open('NL-input.txt', read, Str),
+    read_file(Str,Lines),
+    %Convert the lines in file to an list of sentences that are lists of words
+    lines_to_words(Lines, Words),
+    close(Str),
+    write(Words), nl.
+
+
+% Credit to StackOverflow and author Ishq for file parser
+% https://stackoverflow.com/a/4805931
+% https://stackoverflow.com/users/577045/ishq
+read_file(Stream,[]) :-
+    at_end_of_stream(Stream).
+
+read_file(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream,X),
+    read_file(Stream,L).
+
+%Converts sentence to a list of words
+lines_to_words([], []).
+lines_to_words([H|T], [H2|T2]) :-
+    split_string(H, " ", "", H2),
+    lines_to_words(T, T2).
